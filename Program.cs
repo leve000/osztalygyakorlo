172aa4127bb9c99cd6cd425b1749e57dee44e648
Gyurcsaklj <gyurcsakl@kkszki.hu>
2025-09-24 08:15:16 +0200

    butorlap done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace osztaly
+namespace butorgyar
 {
-    internal class Program
+    public class Butorlap
     {
-        static void Main(string[] args)
+        private int hossz;
+        private int szelesseg;
+        private string tipus;
+
+        public const int AR_TARTOLAP = 5000;
+        public const int AR_HATLAP = 500;
+
+        public Butorlap(string tipus, int hossz, int szelesseg)
         {
+            this.tipus = tipus;
+            this.hossz = hossz;
+            this.szelesseg = szelesseg;
+        }
+
+        public int ArSzamol()
+        {
+            double terulet = (hossz / 100.0) * (szelesseg / 100.0);
+            if (tipus == "tartólap")
+                return (int)(terulet * AR_TARTOLAP);
+            else if (tipus == "hátlap")
+                return (int)(terulet * AR_HATLAP);
+            else
+                return 0;
         }
     }
+
 }
